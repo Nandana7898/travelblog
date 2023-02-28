dc73aa6eaae9166ebbe9e93f8c25e11d79039742
Nandana7898 <124356519+Nandana7898@users.noreply.github.com>
2023-02-28 20:20:36 +0530

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from main.models import Post
+# Create your views here.
+def index(request):
+    postlist=Post.objects.all()
+    return render(request,'index.html',{'postlist':postlist})
+
+def blog(request):
+    postlist=Post.objects.all()
+    return render(request,'blog.html',{'postlist':postlist})
+
+def blogdetails(request,pk):
+    postlist=Post.objects.get(pk=pk)
+    return render(request,'blogdetails.html',{'postlist':postlist})
+
+def about(request):
+    return render(request,'about.html')
