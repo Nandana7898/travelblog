c5e8fae26100d4a7b2bbfe7254929410151b8ab0
Nandana7898 <124356519+Nandana7898@users.noreply.github.com>
2023-03-05 15:35:17 +0530

    Update urls.py

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -14,18 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main.views import index,blog,blogdetails,about
+from django.urls import path, re_path
+from main.views import index, blog, blogdetails, about,gallery
 from django.conf.urls.static import static
 from django.conf import settings
+
+from django.views.static import serve
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',index),
-    path('blog/',blog),
-    path('about/',about),
-    path('blog/<int:pk>/',blogdetails),
-
-
+  re_path(r'^media/(?P<path>.*)$', serve,
+          {'document_root': settings.MEDIA_ROOT}),
+  re_path(r'^static/(?P<path>.*)$', serve,
+          {'document_root': settings.STATIC_ROOT}),
+  path('admin/', admin.site.urls),
+  path('', index),
+  path('blog/', blog),
+  path('about/', about),
+  path('gallery/', gallery),
+  path('blog/<int:pk>/', blogdetails),
 ]
 
-urlpatterns +=static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+  
